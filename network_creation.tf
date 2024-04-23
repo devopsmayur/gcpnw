d32febd58e8ebdbe4530c9fb77cba2db231f9491
Mayur <99334268+devopsmayur@users.noreply.github.com>
2024-04-23 10:21:32 +0530

    Update network_creation.tf

--- network_creation.tf
+++ network_creation.tf
@@ -7,17 +7,10 @@ provider "google" {
 
 resource "google_compute_network" "my_network" {
   name                    = "my-network"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "my_subnet" {
-  name          = "my-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.my_network.self_link
-  region        = "us-central1"
+  auto_create_subnetworks = true
 }
 
 # Output the network information
 output "network_info" {
-  value = google_compute_network.my_network
+  value = google_compute_network.my_network.id
 }
